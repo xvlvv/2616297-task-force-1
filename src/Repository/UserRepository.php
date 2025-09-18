4e3ee9002bacbfa53bc8f6856aacbe45f8001173
xvlvv <vitkovvvvvvv@mail.ru>
2025-09-18 22:17:17 +0200

    feat: implement review, user repository

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Xvlvv\Repository;
+
+use app\models\Review;
+use LogicException;
+use Xvlvv\DTO\UserReviewViewDTO;
+use Xvlvv\DTO\UserSpecializationDTO;
+use Xvlvv\DTO\ViewUserDTO;
+use Xvlvv\Entity\User;
+use \app\models\User as UserModel;
+use Xvlvv\Enums\Status;
+use Xvlvv\Enums\UserRole;
+use yii\db\Expression;
+use yii\db\Query;
+use yii\web\NotFoundHttpException;
+
+class UserRepository implements UserRepositoryInterface
+{
+
+    public function __construct(
+        private ReviewRepositoryInterface $reviewRepo,
+        private TaskRepositoryInterface $taskRepo,
+    ) {
+    }
+
+    public function getById(int $id): ?User
+    {
+        // TODO: Implement getById() method.
+    }
+
+    public function getByIdOrFail(int $id): User
+    {
+        // TODO: Implement getByIdOrFail() method.
+    }
+
+    public function getByEmailOrFail(string $email): User
+    {
+        // TODO: Implement getByEmailOrFail() method.
+    }
+
+    public function update(User $user): void
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function save(User $user): User
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function isUserExistsByEmail(string $email): bool
+    {
+        // TODO: Implement isUserExistsByEmail() method.
+    }
+
+    public function getUserRank(int $userId): int
+    {
+        $ratingsSubQuery = UserModel::find()
+            ->select([
+                'id' => '{{%user}}.id',
+                'avg_rating' => new Expression('IFNULL(AVG({{%review}}.rating), 0)')
+            ])
+            ->leftJoin(Review::tableName(), '{{%review}}.worker_id = {{%user}}.id')
+            ->where(['{{%user}}.role' => UserRole::WORKER])
+            ->groupBy('{{%user}}.id');
+
+        $rankedUsersQuery = (new Query())
+            ->select([
+                'id',
+                'user_rank' => new Expression('RANK() OVER (ORDER BY avg_rating DESC)')
+            ])
+            ->from(['ratings' => $ratingsSubQuery]);
+
+        $finalQuery = (new Query())
+            ->select('user_rank')
+            ->from(['ranked_users' => $rankedUsersQuery])
+            ->where(['id' => $userId]);
+
+        $rank = $finalQuery->scalar();
+
+        if (null === $rank) {
+            throw new LogicException('Cannot calculate rank for not worker type user');
+        }
+
+        return $rank;
+    }
+
+    private function getModelByIdOrFail(int $id, UserRole $role): UserModel
+    {
+        $user = UserModel::findWithRating()->where(['id' => $id, 'role' => $role])->one();
+
+        if (null === $user) {
+            throw new NotFoundHttpException();
+        }
+
+        return $user;
+    }
+
+    public function getWorkerForView(int $id): ViewUserDTO
+    {
+        $user = $this->getModelByIdOrFail($id, UserRole::WORKER);
+
+        $userSpecializations = array_map(
+            function ($categoryModel) {
+                return new UserSpecializationDTO(
+                    $categoryModel->id,
+                    $categoryModel->name,
+                );
+            },
+            $user->specializationCategories
+        );
+
+        $reviews = array_map(
+            function ($reviewModel) {
+                return new UserReviewViewDTO(
+                    $reviewModel->task->name,
+                    $reviewModel->comment,
+                    $reviewModel->rating,
+                    $reviewModel->created_at,
+                    $reviewModel->customer->avatar_path
+                );
+            },
+            $this->reviewRepo->getReviewsByUser($user->id)
+        );
+
+        $workerHasActiveTask = $this->taskRepo->workerHasActiveTask($user->id);
+        $completedTasks = $this->taskRepo->getCompletedTasksCountByWorkerId($user->id);
+        $rank = $this->getUserRank($user->id);
+
+        $status = $workerHasActiveTask ? 'Открыт для новых заказов' : 'Занят';
+
+        return new ViewUserDTO(
+            $user->name,
+            $user->avatar_path,
+            $user->workerProfile->description,
+            $user->workerProfile->bio,
+            $userSpecializations,
+            $completedTasks,
+            $user->failedTasksCount,
+            $user->rating,
+            $rank,
+            $user->created_at,
+            $status,
+            $user->workerProfile->phone_number,
+            $user->email,
+            $user->workerProfile->telegram_username,
+            true,
+            $reviews
+        );
+    }
+}
