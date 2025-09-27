d801b98d01b7fa0395535d411008674d32146e7e
xvlvv <vitkovvvvvvv@mail.ru>
2025-09-27 18:22:10 +0200

    feat: add access control for task response management

---
+++ migrations/m250926_195341_create_task_permission.php
@@ -0,0 +1,107 @@
+<?php
+
+use app\rbac\AuthorRule;
+use app\rbac\TaskAuthorRule;
+use Xvlvv\Enums\UserRole;
+use yii\base\InvalidConfigException;
+use yii\db\Migration;
+use yii\rbac\DbManager;
+
+class m250926_195341_create_task_permission extends Migration
+{
+    public function getAuthManager(): DbManager
+    {
+        $auth = Yii::$app->authManager;
+
+        if (!$auth instanceof DbManager) {
+            throw new InvalidConfigException(
+                'You should configure "authManager" component to use database before executing this migration.'
+            );
+        }
+
+        return $auth;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp(): void
+    {
+        $auth = $this->getAuthManager();
+
+        $workerRole = $auth->getRole(UserRole::WORKER->value);
+
+        if (null === $workerRole) {
+            throw new RuntimeException('User role not found');
+        }
+
+        $customerRole = $auth->getRole(UserRole::CUSTOMER->value);
+
+        if (null === $customerRole) {
+            throw new RuntimeException('User role not found');
+        }
+
+        $taskAuthorRule = new TaskAuthorRule();
+        $auth->add($taskAuthorRule);
+
+        $manageTaskResponses = $auth->createPermission('manageTaskResponses');
+        $manageTaskResponses->ruleName = $taskAuthorRule->name;
+        $auth->add($manageTaskResponses);
+        $auth->addChild($customerRole, $manageTaskResponses);
+
+        $applyToTask = $auth->createPermission('applyToTask');
+        $auth->add($applyToTask);
+
+        $auth->addChild($workerRole, $applyToTask);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown(): void
+    {
+        $auth = $this->getAuthManager();
+        $applyToTask = $auth->getPermission('applyToTask');
+
+        if (null === $applyToTask) {
+            throw new RuntimeException('Permission not found');
+        }
+
+        $manageTaskResponses = $auth->getPermission('manageTaskResponses');
+
+        if (null === $manageTaskResponses) {
+            throw new RuntimeException('Permission not found');
+        }
+
+        $workerRole = $auth->getRole(UserRole::WORKER->value);
+
+        if (null === $workerRole) {
+            throw new RuntimeException('User role not found');
+        }
+
+        $customerRole = $auth->getRole(UserRole::CUSTOMER->value);
+
+        if (null === $customerRole) {
+            throw new RuntimeException('User role not found');
+        }
+
+        $auth->removeChild($workerRole, $applyToTask);
+        $auth->remove($applyToTask);
+
+        $manageTaskResponses = $auth->getPermission('manageTaskResponses');
+
+        if (null === $manageTaskResponses) {
+            throw new RuntimeException('Permission not found');
+        }
+
+        $auth->remove($manageTaskResponses);
+
+        $taskAuthorRule = $auth->getRule('isTaskAuthor');
+
+        if (null === $taskAuthorRule) {
+            throw new RuntimeException('Rule not found');
+        }
+
+        $auth->remove($taskAuthorRule);
+    }
+}
--- rbac/TaskAuthorRule.php
+++ rbac/TaskAuthorRule.php
@@ -2,6 +2,7 @@
 
 namespace app\rbac;
 
+use Xvlvv\Repository\TaskRepositoryInterface;
 use Yii;
 use yii\base\InvalidConfigException;
 use yii\rbac\Rule;
@@ -15,7 +16,13 @@ class TaskAuthorRule extends Rule
      */
     public function execute($user, $item, $params): bool
     {
-        $userRepo = Yii::$app->get('UserRepositoryInterface');
-        return $userRepo->isAuthor((int) $user);
+        $taskRepo = Yii::$container->get(TaskRepositoryInterface::class);
+        $taskId = $params['taskId'] ?? null;
+
+        if (null === $taskId) {
+            return false;
+        }
+
+        return $taskRepo->isAuthor($taskId, (int) $user);
     }
 }
